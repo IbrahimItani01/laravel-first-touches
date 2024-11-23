98751d86efd5e2e16c556ebab08c75aaec7822c5
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-23 16:27:20 +0200

    php<>created the sub routes for admins apis

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::group(["prefix"=> "/admin"], function () {
-    
+    Route::post("/",AdminController::class."postNews");
+    Route::put("/",AdminController::class."editNews");
+    Route::put("/{id}",AdminController::class."restrictNews");
+    Route::delete("/{id}",AdminController::class."deleteNews");
 });
